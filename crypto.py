42d7b5e109f9a60198cbfde566d0e5cc3e5da7d3
Jake Mooney <jrmooney@gmail.com>
2019-08-01 09:21:35 -0400

    it works, no errors, everything connects

--- crypto.py
+++ crypto.py
@@ -1,7 +1,7 @@
 # import requests
 from coinapi_v1 import CoinAPIv1
 import datetime
-import SQLAlchemy
+import sqlalchemy
 import numpy as np
 import psycopg2
 import pandas as pd
@@ -9,10 +9,8 @@ import pandas as pd
 test_key = 'E2B37F93-9B44-4E58-8E38-1F9B6A31A8D0'
 
 
-conn_string = "port=5432 dbname=crypto user=postgres password=password"
-conn=psycopg2.connect(conn_string)
 api = CoinAPIv1(test_key)
-db = SQLAlchemy.create_engine(conn_string)
+db = sqlalchemy.create_engine('postgresql://postgres:password@localhost/crypto')
 engine = db.connect()
 
 def get_year_of_data():
@@ -21,21 +19,24 @@ def get_year_of_data():
     symbols = ["BTC", "ETH", "XRP", "DOGE"]
     if crypto_symbol not in symbols:
         print("Please select a symbol from this list: ")
-        print(len(symbols))
         get_year_of_data()
     else:
         today = datetime.datetime.today()
         start_time = datetime.date(today.year-1, today.month, today.day).isoformat()
-        # For the purpose of thos excercise we are using USD as a pricing currency. This could
+        # For the purpose of those excercise we are using USD as a pricing currency. This could
         # also be made customizable.
         asset_id_quote = 'USD'
+        # asset_id_quote = input("Currency: ")
         asset_id_base = crypto_symbol
         exchange_id = 'BITSTAMP'
         symbol_type = f'{exchange_id}_SPOT_{asset_id_base}_{asset_id_quote}'
         ohlcv_historical = api.ohlcv_historical_data(symbol_type, {'period_id': '1MIN', 'time_start': start_time})
         # ohlcv_historical['asset_id_base'] = ohlcv_historical
-        print(type(ohlcv_historical))
-        print(ohlcv_historical)
         data = pd.DataFrame(ohlcv_historical)
         data.to_sql(name='crypto', con=engine, if_exists = 'replace', index=False)
 get_year_of_data()
+
+
+
+
+    
