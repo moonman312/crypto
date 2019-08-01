7d421cf82a28e7688f6ac06e965bdd453099914b
Jake Mooney <jrmooney@gmail.com>
2019-08-01 08:45:49 -0400

    everything should be working. Still getting a dependency error

--- crypto.py
+++ crypto.py
@@ -3,6 +3,8 @@ from coinapi_v1 import CoinAPIv1
 import datetime
 import SQLAlchemy
 import numpy as np
+import psycopg2
+import pandas as pd
 
 test_key = 'E2B37F93-9B44-4E58-8E38-1F9B6A31A8D0'
 
@@ -10,19 +12,20 @@ test_key = 'E2B37F93-9B44-4E58-8E38-1F9B6A31A8D0'
 conn_string = "port=5432 dbname=crypto user=postgres password=password"
 conn=psycopg2.connect(conn_string)
 api = CoinAPIv1(test_key)
-db = sqlalchemy.create_engine(conn_string)
+db = SQLAlchemy.create_engine(conn_string)
 engine = db.connect()
 
 def get_year_of_data():
     crypto_symbol = input("Enter a crypto symbol (BTC, ETH, XRP, DOGE): ")
-    symbols = api.metadata_list_symbols()
+    # symbols = api.metadata_list_symbols()
+    symbols = ["BTC", "ETH", "XRP", "DOGE"]
     if crypto_symbol not in symbols:
         print("Please select a symbol from this list: ")
-        print(symbols)
+        print(len(symbols))
         get_year_of_data()
     else:
-        datetime = datetime.datetime.today()
-        start_time = datetime.date(datetime.year-1, datetime.month, datetime.day).isoformat()
+        today = datetime.datetime.today()
+        start_time = datetime.date(today.year-1, today.month, today.day).isoformat()
         # For the purpose of thos excercise we are using USD as a pricing currency. This could
         # also be made customizable.
         asset_id_quote = 'USD'
@@ -30,21 +33,9 @@ def get_year_of_data():
         exchange_id = 'BITSTAMP'
         symbol_type = f'{exchange_id}_SPOT_{asset_id_base}_{asset_id_quote}'
         ohlcv_historical = api.ohlcv_historical_data(symbol_type, {'period_id': '1MIN', 'time_start': start_time})
-        ohlcv_historical['asset_id_base'] = ohlcv_historical
-        data = pd.read_json(ohlcv_historical, orient='records', dtype={
-            'time_close': np.datetime64,
-            'time_open': np.datetime64,
-            'time_period_end': np.datetime64,
-            'time_period_start': np.datetime64,
-            "time_period_start": np.datetime64,
-            "time_period_end": np.datetime64,
-            "time_open": np.datetime64,
-            "time_close": np.datetime64,
-            "price_open": np.float64,
-            "price_high": np.float64,
-            "price_low": np.float64,
-            "price_close": np.float64,
-            "volume_traded": np.float64,
-            "trades_count": np.int64
-        })
+        # ohlcv_historical['asset_id_base'] = ohlcv_historical
+        print(type(ohlcv_historical))
+        print(ohlcv_historical)
+        data = pd.DataFrame(ohlcv_historical)
         data.to_sql(name='crypto', con=engine, if_exists = 'replace', index=False)
+get_year_of_data()
