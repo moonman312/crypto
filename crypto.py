280d8ec08adb92182eae3e52c657f6f4f7772b49
moonman312 <jrmooney@gmail.com>
2019-08-01 12:31:49 -0400

    reading from db working

--- crypto.py
+++ crypto.py
@@ -5,13 +5,15 @@ import sqlalchemy
 import numpy as np
 import psycopg2
 import pandas as pd
+from matplotlib import pyplot
 
 test_key = 'E2B37F93-9B44-4E58-8E38-1F9B6A31A8D0'
 
 
 api = CoinAPIv1(test_key)
-db = sqlalchemy.create_engine('postgresql://postgres:password@localhost/crypto')
+db = sqlalchemy.create_engine('postgresql://jmooney@localhost/crypto')
 engine = db.connect()
+
 
 def get_year_of_data():
     crypto_symbol = input("Enter a crypto symbol (BTC, ETH, XRP, DOGE): ")
@@ -34,9 +36,27 @@ def get_year_of_data():
         # ohlcv_historical['asset_id_base'] = ohlcv_historical
         data = pd.DataFrame(ohlcv_historical)
         data.to_sql(name='crypto', con=engine, if_exists = 'replace', index=False)
+
 get_year_of_data()
 
 
+def read_data():
+    query = "SELECT * FROM crypto"
+    df = pd.read_sql(query, engine)
+    #Remove UTC timezone information
+    df['time_open'] = pd.to_datetime(df['time_open']).dt.tz_localize(None)
+    df['time_close'] = pd.to_datetime(df['time_close']).dt.tz_localize(None)
+    df['time_period_start'] = pd.to_datetime(df['time_period_start']).dt.tz_localize(None)
+    df['time_period_end'] = pd.to_datetime(df['time_period_end']).dt.tz_localize(None)
+    #Convert timestampts to numeric values (seconds since epoch time)
+    df['time_open_epoch'] = (df['time_open'] - datetime.datetime(1970,1,1)).dt.total_seconds()
+    df['time_close_epoch'] = (df['time_close'] - datetime.datetime(1970,1,1)).dt.total_seconds()
+    df['time_period_start_epoch'] = (df['time_period_start'] - datetime.datetime(1970,1,1)).dt.total_seconds()
+    df['time_period_end_epoch'] = (df['time_period_end'] - datetime.datetime(1970,1,1)).dt.total_seconds()
+    df.plot(kind='scatter', x='epoch', y='price_open')
+    pyplot.show()
+    print(df.columns)
 
+    return
 
-    
+read_data()
